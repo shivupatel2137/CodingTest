7d0257ca216aaaf966290f8fe60e12bbf7c90a24
shivupatel2137 <shivu.patel2137@gmail.com>
2020-09-09 16:04:16 +0530

    Scenario for SKU A and B

--- ProblemStatement1/Program.cs
+++ ProblemStatement1/Program.cs
@@ -30,7 +30,28 @@ namespace ProblemStatement1
             }
             foreach (string sku in Ids)
             {
-               
+                int val;
+                val = Convert.ToInt32(sku.Substring(0, 1));
+                if (sku.ToUpper().Contains("A"))
+                {
+                    if (val >= 3)
+                    {
+                        int i = val / 3;
+                        sum += i * sumA;
+                        val -= i * 3;
+                    }
+                    sum += val * a;
+                }
+                else if (sku.ToUpper().Contains("B"))
+                {
+                    if (val >= 2)
+                    {
+                        int i = val / 2;
+                        sum += i * SumB;
+                        val -= i * 2;
+                    }
+                    sum += val * b;
+                }
             }
             return "Total : " + sum;
         }
