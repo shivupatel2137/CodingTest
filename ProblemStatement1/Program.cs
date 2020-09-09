ae814e674d766f019734136ade45a96278e999c7
shivupatel2137 <shivu.patel2137@gmail.com>
2020-09-09 17:18:59 +0530

    Validations

--- ProblemStatement1/Program.cs
+++ ProblemStatement1/Program.cs
@@ -20,72 +20,82 @@ namespace ProblemStatement1
             a = 50; b = 30; c = 20; d = 15;
             sumA = 130; SumB = 45; CnD = 30;
             Console.WriteLine("Enter no(N) of items required for SKU");
-            int id = Convert.ToInt32(Console.ReadLine());
+            int id = 0;
             int sum = 0;
-            string[] Ids = new string[id];
-            Console.WriteLine("Enter Value for SKU Id: ");
-            for (int i = 0; i < id; i++)
+            string s = Console.ReadLine();
+            bool result = int.TryParse(s, out id);
+            if(result == true)
             {
-                Ids[i] = Console.ReadLine();
+                string[] Ids = new string[id];
+                Console.WriteLine("Enter Value for SKU Id: ");
+                for (int i = 0; i < id; i++)
+                {
+                    Ids[i] = Console.ReadLine();
+                }
+                foreach (string sku in Ids)
+                {
+                    int val;
+                    val = Convert.ToInt32(sku.Substring(0, 1));
+                    if (sku.ToUpper().Contains("A"))
+                    {
+                        if (val >= 3)
+                        {
+                            int i = val / 3;
+                            sum += i * sumA;
+                            val -= i * 3;
+                        }
+                        sum += val * a;
+                    }
+                    else if (sku.ToUpper().Contains("B"))
+                    {
+                        if (val >= 2)
+                        {
+                            int i = val / 2;
+                            sum += i * SumB;
+                            val -= i * 2;
+                        }
+                        sum += val * b;
+                    }
+                    else if (sku.ToUpper().Contains("C"))
+                    {
+                        if (!Ids.Any(x => x.ToUpper().Contains("D")))
+                            sum += val * c;
+                    }
+                    else if (sku.ToUpper().Contains("D"))
+                    {
+                        if (Ids.Any(x => x.ToUpper().Contains("C")))
+                        {
+                            int Cval = Convert.ToInt32(Ids.Where(x => x.ToUpper().Contains("C")).FirstOrDefault().ToString().Substring(0, 1));
+                            if (val == Cval)
+                            {
+                                sum += val * CnD;
+                            }
+                            else if (val > Cval)
+                            {
+                                int i = val - Cval;
+                                sum += Cval * CnD;
+                                val -= i;
+                                sum += i * d;
+                            }
+                            else if (Cval > val)
+                            {
+                                int i = Cval - val;
+                                sum += val * CnD;
+                                val -= i;
+                                sum += i * c;
+                            }
+                        }
+                        else
+                        {
+                            sum += val * d;
+                        }
+                    }
+                }
             }
-            foreach (string sku in Ids)
+            else
             {
-                int val;
-                val = Convert.ToInt32(sku.Substring(0, 1));
-                if (sku.ToUpper().Contains("A"))
-                {
-                    if (val >= 3)
-                    {
-                        int i = val / 3;
-                        sum += i * sumA;
-                        val -= i * 3;
-                    }
-                    sum += val * a;
-                }
-                else if (sku.ToUpper().Contains("B"))
-                {
-                    if (val >= 2)
-                    {
-                        int i = val / 2;
-                        sum += i * SumB;
-                        val -= i * 2;
-                    }
-                    sum += val * b;
-                }
-                else if (sku.ToUpper().Contains("C"))
-                {
-                    if (!Ids.Any(x => x.ToUpper().Contains("D")))
-                        sum += val * c;
-                }
-                else if (sku.ToUpper().Contains("D"))
-                {
-                    if (Ids.Any(x => x.ToUpper().Contains("C")))
-                    {
-                        int Cval = Convert.ToInt32(Ids.Where(x => x.ToUpper().Contains("C")).FirstOrDefault().ToString().Substring(0, 1));
-                        if (val == Cval)
-                        {
-                            sum += val * CnD;
-                        }
-                        else if (val > Cval)
-                        {
-                            int i = val - Cval;
-                            sum += Cval * CnD;
-                            val -= i;
-                            sum += i * d;
-                        }
-                        else if (Cval > val)
-                        {
-                            int i = Cval - val;
-                            sum += val * CnD;
-                            val -= i;
-                            sum += i * c;
-                        }
-                    }
-                    else
-                    {
-                        sum += val * d;
-                    }
-                }
+                Console.WriteLine("Please Enter numeric value for Number of SKU.");
+                ChkProc();
             }
             return "Total : " + sum;
         }
